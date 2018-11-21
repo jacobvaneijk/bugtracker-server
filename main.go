7dd4efef59992b01c2d580260e0b2b5cd34444cb
Jacob van Eijk <jacob.vaneijk@gmail.com>
2018-11-21 10:52:29 +0100

    Add possibility to add simple cards (title + description) to a Trello board.

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "os"
+    "log"
+    "fmt"
+
+    "github.com/gorilla/mux"
+    "github.com/joho/godotenv"
+)
+
+func CreateBugHandler(w http.ResponseWriter, r *http.Request) {
+    req, err := http.NewRequest(
+        "POST",
+        os.Getenv("TRELLO_API_URL"),
+        nil,
+    )
+
+    if err != nil {
+        fmt.Printf("http.NewRequest() error: %v\n", err)
+        return
+    }
+
+    q := req.URL.Query()
+
+    q.Add("key", os.Getenv("TRELLO_API_KEY"))
+    q.Add("token", os.Getenv("TRELLO_API_TOKEN"))
+    q.Add("name", r.FormValue("title"))
+    q.Add("desc", r.FormValue("description"))
+    q.Add("idList", r.FormValue("list"))
+
+    req.URL.RawQuery = q.Encode()
+    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+    c := &http.Client{}
+    resp, err := c.Do(req)
+
+    if err != nil {
+        fmt.Printf("http.Do() error: %v\n", err)
+        return
+    }
+
+    defer resp.Body.Close()
+
+    fmt.Fprintf(w, "Created new card!");
+}
 
 func main() {
-    fmt.Printf("Hello, world!\n");
+    err := godotenv.Load()
+
+    if err != nil {
+        log.Fatal("Couldn't read `.env` file.")
+    }
+
+    r := mux.NewRouter()
+
+    r.HandleFunc("/bug", CreateBugHandler).Methods("POST")
+
+    http.Handle("/", r)
+
+    log.Fatal(http.ListenAndServe(":4321", nil))
 }
